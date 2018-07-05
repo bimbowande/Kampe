feb67e2ad49cc3d416a0f395064ccaa8a96fe943
Oluwagbuyi Abimbola <bimbowande@gmail.com>
2018-07-05 17:06:07 +0100

    file-update

--- src/components/App.js
+++ src/components/App.js
@@ -4,9 +4,7 @@ import Bg from './Bg';
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Welcome to Kampe Sport Group</h1>
-      </div>
+      <Bg/>
     )
   }
 }
--- src/components/Bg.js
+++ src/components/Bg.js
@@ -5,11 +5,11 @@ class Bg extends Component {
     render() {
         return (
             <div className="bg-section">
-                <div className="bg-subsection bg-subsection1"></div>
-                <div className="bg-subsection bg-subsection2"></div>
-                <div className="bg-subsection bg-subsection3"></div>
-                <div className="bg-subsection bg-subsection4 "></div>
-                <div className="bg-subsection bg-subsection5"></div>
+                <div className="bg-subsection bg-subsection1">1</div>
+                <div className="bg-subsection bg-subsection2">2</div>
+                <div className="bg-subsection bg-subsection3">3</div>
+                <div className="bg-subsection bg-subsection4 ">4</div>
+                <div className="bg-subsection bg-subsection5">5</div>
             </div>
         );
     }
