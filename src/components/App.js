4f9c88d1218e501164d7c8ed194b216adaaeb9b6
Oluwagbuyi Abimbola <bimbowande@gmail.com>
2018-07-05 14:15:53 +0100

    start

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>Welcome to My Starter App</h1>
+        <h1>Welcome to Kampe Sport Group</h1>
       </div>
     )
   }
