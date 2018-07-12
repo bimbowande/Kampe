bd0cd297098cc246fde00e3d5af2613e54cfc11f
Oluwagbuyi Abimbola <bimbowande@gmail.com>
2018-07-12 13:55:11 +0100

    Merge branch 'feature/next-cofigure' into develop

--- index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../src/components/App';
-import '../src/styles/reset.scss';
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('app')
-)
