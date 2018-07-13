3f27ea90fd914339c1a7e17e4a1e320603351940
Oluwagbuyi Abimbola <bimbowande@gmail.com>
2018-07-13 15:55:04 +0100

    done

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,8 +5,8 @@ import BrandName from './BrandName';
   return (
     <div className={className}>
       <BrandName name="Kampe" className="form-brandname"/>
-      <TextInput className='input-group'/>
-      <TextInput className='input-group'/>
+      <TextInput className='input-group' inputclass='input-box' iconname='user' placeholder='Username or email'/>
+      <TextInput type='password' className='input-group' inputclass='input-box' iconname='key' placeholder='Password'/>
     </div>
   )
 }
--- src/components/TextInput.js
+++ src/components/TextInput.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const TextInput = ({className,iconclass,type,inputclass,placeholder}) => {
+const TextInput = ({className,iconclass,type,inputclass,placeholder,iconname}) => {
   return (
     <div className={className}>
-      <i className={iconclass}></i>
+      <i><FontAwesomeIcon icon={iconname} size="1x"/></i>
       <input type={type} className={inputclass} placeholder={placeholder} />
     </div>
   )
